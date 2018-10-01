e239821f2d53e6d7b60643f615427721a83ad589
Bryan Housel <bryan@mapbox.com>
2018-10-01 12:07:19 -0400

    Upgrade to fontawesome v5.3 and fontawesome-svg-core v1.2

--- build_icons.js
+++ build_icons.js
@@ -2,19 +2,19 @@
 const colors = require('colors/safe');
 const fs = require('fs');
 
-const fontawesome = require('@fortawesome/fontawesome');
-const faAt = require('@fortawesome/fontawesome-free-solid/faAt');
-const faComments = require('@fortawesome/fontawesome-free-solid/faComments');
-const faDiscord = require('@fortawesome/fontawesome-free-brands/faDiscord');
-const faDiscourse = require('@fortawesome/fontawesome-free-brands/faDiscourse');
-const faFacebook = require('@fortawesome/fontawesome-free-brands/faFacebook');
-const faKeyboard = require('@fortawesome/fontawesome-free-solid/faKeyboard');
-const faMeetup = require('@fortawesome/fontawesome-free-brands/faMeetup');
-const faReddit = require('@fortawesome/fontawesome-free-brands/faReddit');
-const faSlack = require('@fortawesome/fontawesome-free-brands/faSlack');
-const faTelegram = require('@fortawesome/fontawesome-free-brands/faTelegram');
-const faTwitter = require('@fortawesome/fontawesome-free-brands/faTwitter');
-const faUsers = require('@fortawesome/fontawesome-free-solid/faUsers');
+const fontawesome = require('@fortawesome/fontawesome-svg-core');
+const faAt = require('@fortawesome/free-solid-svg-icons/faAt').faAt;
+const faComments = require('@fortawesome/free-solid-svg-icons/faComments').faComments;
+const faDiscord = require('@fortawesome/free-brands-svg-icons/faDiscord').faDiscord;
+const faDiscourse = require('@fortawesome/free-brands-svg-icons/faDiscourse').faDiscourse;
+const faFacebook = require('@fortawesome/free-brands-svg-icons/faFacebook').faFacebook;
+const faKeyboard = require('@fortawesome/free-solid-svg-icons/faKeyboard').faKeyboard;
+const faMeetup = require('@fortawesome/free-brands-svg-icons/faMeetup').faMeetup;
+const faReddit = require('@fortawesome/free-brands-svg-icons/faReddit').faReddit;
+const faSlack = require('@fortawesome/free-brands-svg-icons/faSlack').faSlack;
+const faTelegram = require('@fortawesome/free-brands-svg-icons/faTelegram').faTelegram;
+const faTwitter = require('@fortawesome/free-brands-svg-icons/faTwitter').faTwitter;
+const faUsers = require('@fortawesome/free-solid-svg-icons/faUsers').faUsers;
 
 buildAll();
 
@@ -47,4 +47,3 @@ function buildAll() {
 
     console.timeEnd(colors.green('icons built'));
 }
-
