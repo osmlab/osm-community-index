8b9e942327905ecdc290e6ca3270ae2673d8ce3a
Bryan Housel <bryan@mapbox.com>
2019-10-14 18:05:38 -0400

    Make `npm run stats` sort by size descending

--- stats.js
+++ stats.js
@@ -9,31 +9,37 @@ const Table = require('easy-table');
 getStats();
 
 function getStats() {
-    let t = new Table;
-    glob.sync(__dirname + '/features/**/*.geojson').forEach(addRow);
-    console.log(t.toString());
+  let t = new Table;
+  glob.sync(__dirname + '/features/**/*.geojson').forEach(addRow);
+  t.sort(['Size|des']);
+  console.log(t.toString());
 
-    t = new Table;
-    glob.sync(__dirname + '/resources/**/*.json').forEach(addRow);
-    console.log(t.toString());
+  t = new Table;
+  glob.sync(__dirname + '/resources/**/*.json').forEach(addRow);
+  t.sort(['Size|des']);
+  console.log(t.toString());
 
 
-    function addRow(file) {
-        let stats = fs.statSync(file);
-        let color = colorBytes(stats.size);
-        t.cell('Size', color(bytes(stats.size, { unitSeparator: ' ' })));
-        t.cell('File', color(path.basename(file)));
-        t.newRow();
+  function addRow(file) {
+    let stats = fs.statSync(file);
+    let color = colorBytes(stats.size);
+
+    t.cell('Size', stats.size, function sizePrinter(val, width) {
+      let displaySize = bytes(stats.size, { unitSeparator: ' ' });
+      return width ? Table.padLeft(displaySize, width) : color(displaySize);
+    });
+    t.cell('File', color(path.basename(file)));
+    t.newRow();
+  }
+
+  function colorBytes(size) {
+    if (size > 1024 * 10) {  // 10 KB
+      return colors.red;
+    } else if (size > 1024 * 2) {  // 2 KB
+      return colors.yellow;
+    } else {
+      return colors.green;
     }
-
-    function colorBytes(size) {
-        if (size > 1024 * 10) {  // 10 KB
-            return colors.red;
-        } else if (size > 1024 * 2) {  // 2 KB
-            return colors.yellow;
-        } else {
-            return colors.green;
-        }
-    }
+  }
 }
 
