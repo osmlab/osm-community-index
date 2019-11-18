9af5aed777e11ef47379d73f948d827018a065a2
Bryan Housel <bryan@mapbox.com>
2019-11-18 09:37:46 -0500

    Export isValidLocation and locationToFeature

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
+import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-    input: 'index.mjs',
-    output: {
-        name: 'oci',
-        file: 'dist/index.js',
-        format: 'umd'
-    },
-    plugins: [
-        json({ indent: '' })
-    ]
+  input: 'index.mjs',
+  output: {
+    name: 'oci',
+    file: 'dist/index.js',
+    format: 'umd'
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json({ indent: '' })
+  ]
 };
