0dadd39e05b7903b59a8f8a6fe06d39cc2404fa5
Bryan Housel <bryan@mapbox.com>
2019-11-18 13:33:34 -0500

    Add tests for locationToFeature and isValidLocation

---
+++ test/locationToFeature.test.js
@@ -0,0 +1,55 @@
+const test = require('tap').test;
+const oci = require('../.');
+
+
+test('locationToFeature', t => {
+
+  t.test('points', t => {
+    t.test('a valid [lon,lat] coordinate pair returns a feature match', t => {
+      let result = oci.locationToFeature([0, 0]);
+      t.notEqual(result, null);
+      t.equal(result.type, 'point');
+      t.end();
+    });
+    t.test('an invalid [lon,lat] coordinate pair returns a null match', t => {
+      let result = oci.locationToFeature([]);
+      t.equal(result, null);
+      t.end();
+    });
+    t.end();
+  });
+
+
+  t.test('`.geojson` filenames', t => {
+    t.test('a valid `.geojson` filename in this project returns a feature match', t => {
+      let result = oci.locationToFeature('philly_metro.geojson', oci.features);
+      t.notEqual(result, null);
+      t.equal(result.type, 'geojson');
+      t.end();
+    });
+    t.test('an invalid `.geojson` filename in this project returns a null match', t => {
+      let result = oci.locationToFeature('fake.geojson', oci.features);
+      t.equal(result, null);
+      t.end();
+    });
+    t.end();
+  });
+
+
+  t.test('country coder feature identifiers', t => {
+    t.test('a valid country coder feature identifier returns a feature match', t => {
+      let result = oci.locationToFeature('gb');
+      t.notEqual(result, null);
+      t.equal(result.type, 'countrycoder');
+      t.end();
+    });
+    t.test('an invalid country coder feature identifier returns a null match', t => {
+      let result = oci.locationToFeature('fake');
+      t.equal(result, null);
+      t.end();
+    });
+    t.end();
+  });
+
+  t.end();
+});
