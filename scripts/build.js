a36f327f6f6ee34121563d49a193bc00e3989bdd
Bryan Housel <bhousel@gmail.com>
2021-04-12 15:53:22 -0400

    move some common functions to the lib/ folder

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,5 @@
 const calcArea = require('@mapbox/geojson-area');
 const colors = require('colors/safe');
-const diacritics = require('diacritics');
 const fs = require('fs');
 const glob = require('glob');
 const JSON5 = require('json5');
@@ -13,6 +12,10 @@ const stringify = require('@aitodotai/json-stringify-pretty-compact');
 const Validator = require('jsonschema').Validator;
 const withLocale = require('locale-compare')('en-US');
 const YAML = require('js-yaml');
+
+const resolveStrings = require('../lib/resolve_strings.js');
+const sortObject = require('../lib/sort_object.js');
+const simplify = require('../lib/simplify.js');
 
 const geojsonSchema = require('../schema/geojson.json');
 const featureSchema = require('../schema/feature.json');
@@ -53,12 +56,12 @@ function buildAll() {
 
   // Resources
   _resources = collectResources(featureCollection);
-  fs.writeFileSync('dist/resources.json', stringify({ resources: sort(_resources) }, { maxLength: 9999 }) + '\n');
+  fs.writeFileSync('dist/resources.json', stringify({ resources: sortObject(_resources) }, { maxLength: 9999 }) + '\n');
 
   // Translations
-  _tstrings._defaults = sort(_tstrings._defaults);
-  _tstrings._communities = sort(_tstrings._communities);
-  fs.writeFileSync('i18n/en.yaml', YAML.dump({ en: sort(_tstrings) }, { lineWidth: -1 }) );
+  _tstrings._defaults = sortObject(_tstrings._defaults);
+  _tstrings._communities = sortObject(_tstrings._communities);
+  fs.writeFileSync('i18n/en.yaml', YAML.dump({ en: sortObject(_tstrings) }, { lineWidth: -1 }) );
 
   console.timeEnd(END);
 }
@@ -259,7 +262,15 @@ function collectResources(featureCollection) {
       translateStrings.extendedDescription = item.strings.extendedDescription;
     }
 
-    const resolvedStrings = resolveStrings(item);
+    let resolvedStrings;
+    try {
+      resolvedStrings = resolveStrings(item, _defaults);
+    } catch (e) {
+      console.error(colors.red(e + ': ') + colors.yellow(itemID));
+      console.error('  ' + colors.yellow(file));
+      process.exit(1);
+    }
+
     if (!resolvedStrings.name) {
       console.error(colors.red('Error - Cannot resolve a name for resource: ') + colors.yellow(itemID));
       console.error('  ' + colors.yellow(file));
@@ -342,73 +353,4 @@ function prettifyFile(file, object, contents) {
   if (pretty !== contents) {
     fs.writeFileSync(file, pretty);
   }
-}
-
-
-// Resolves string token replacements, fail if any are unresolvable
-function resolveStrings(item) {
-  const itemStrings = item.strings || {};
-  const defaultStrings = _defaults[item.type] || {};
-
-  const replacements = {
-    account: item.account,
-    community: itemStrings.community,
-    signupUrl: item.signupUrl,
-    url: item.url
-  };
-
-  const resolved = {
-    name:                resolve(itemStrings.name || defaultStrings.name),
-    description:         resolve(itemStrings.description || defaultStrings.description),
-    extendedDescription: resolve(itemStrings.extendedDescription || defaultStrings.extendedDescription),
-    signupUrl:           resolve(item.signupUrl || defaultStrings.signupUrl),
-    url:                 resolve(item.url || defaultStrings.url)
-  };
-
-  return resolved;
-
-  function resolve(s) {
-    if (!s) return null;
-    let result = s;
-
-    for (let key in replacements) {
-      const token = `{${key}}`;
-      const regex = new RegExp(token, 'g');
-      if (regex.test(result)) {
-        const val = replacements[key];
-        if (!val) {
-          console.error(colors.red(`Error - Cannot resolve ${token} for resource: `) + colors.yellow(item.id));
-          process.exit(1);
-        } else {
-          result = result.replace(regex, val);
-        }
-      }
-    }
-    return result;
-  }
-}
-
-
-// Returns an object with sorted keys and sorted values.
-// (This is useful for file diffing)
-function sort(obj) {
-  if (!obj) return null;
-
-  let sorted = {};
-  Object.keys(obj).sort(withLocale).forEach(k => {
-    sorted[k] = Array.isArray(obj[k]) ? obj[k].sort(withLocale) : obj[k];
-  });
-  return sorted;
-}
-
-// Returns a string with diacritics, spaces, punctuation removed
-function simplify(str) {
-  if (typeof str !== 'string') return '';
-  return diacritics.remove(
-    str
-      .replace(/&/g, 'and')
-      .replace(/İ/ig, 'i')
-      .replace(/[\s\-=_!"#%'*{},.\/:;?\(\)\[\]@\\$\^*+<>«»~`’\u00a1\u00a7\u00b6\u00b7\u00bf\u037e\u0387\u055a-\u055f\u0589\u05c0\u05c3\u05c6\u05f3\u05f4\u0609\u060a\u060c\u060d\u061b\u061e\u061f\u066a-\u066d\u06d4\u0700-\u070d\u07f7-\u07f9\u0830-\u083e\u085e\u0964\u0965\u0970\u0af0\u0df4\u0e4f\u0e5a\u0e5b\u0f04-\u0f12\u0f14\u0f85\u0fd0-\u0fd4\u0fd9\u0fda\u104a-\u104f\u10fb\u1360-\u1368\u166d\u166e\u16eb-\u16ed\u1735\u1736\u17d4-\u17d6\u17d8-\u17da\u1800-\u1805\u1807-\u180a\u1944\u1945\u1a1e\u1a1f\u1aa0-\u1aa6\u1aa8-\u1aad\u1b5a-\u1b60\u1bfc-\u1bff\u1c3b-\u1c3f\u1c7e\u1c7f\u1cc0-\u1cc7\u1cd3\u200b-\u200f\u2016\u2017\u2020-\u2027\u2030-\u2038\u203b-\u203e\u2041-\u2043\u2047-\u2051\u2053\u2055-\u205e\u2cf9-\u2cfc\u2cfe\u2cff\u2d70\u2e00\u2e01\u2e06-\u2e08\u2e0b\u2e0e-\u2e16\u2e18\u2e19\u2e1b\u2e1e\u2e1f\u2e2a-\u2e2e\u2e30-\u2e39\u3001-\u3003\u303d\u30fb\ua4fe\ua4ff\ua60d-\ua60f\ua673\ua67e\ua6f2-\ua6f7\ua874-\ua877\ua8ce\ua8cf\ua8f8-\ua8fa\ua92e\ua92f\ua95f\ua9c1-\ua9cd\ua9de\ua9df\uaa5c-\uaa5f\uaade\uaadf\uaaf0\uaaf1\uabeb\ufe10-\ufe16\ufe19\ufe30\ufe45\ufe46\ufe49-\ufe4c\ufe50-\ufe52\ufe54-\ufe57\ufe5f-\ufe61\ufe68\ufe6a\ufe6b\ufeff\uff01-\uff03\uff05-\uff07\uff0a\uff0c\uff0e\uff0f\uff1a\uff1b\uff1f\uff20\uff3c\uff61\uff64\uff65]+/g,'')
-      .toLowerCase()
-  );
 }
