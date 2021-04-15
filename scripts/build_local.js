6fba09752360c6d3a237fd855e2fc786ad61293d
Bryan Housel <bhousel@gmail.com>
2021-04-14 22:47:02 -0400

    For docs-local, use fs-extra to ensure directory exists

--- scripts/build_local.js
+++ scripts/build_local.js
@@ -1,17 +1,18 @@
-const fs = require("fs");
+const fs = require('fs-extra');
 
 buildLocal();
 
 function buildLocal() {
-  if (fs.existsSync('docs-local')) {
-    var stats = fs.statSync("dist/completeFeatureCollection.min.json");
-    var mtime = stats.mtime;
-    let indexDocsRemote = fs.readFileSync('docs/index.html').toString();
-    let indexDocsLocal = 
-      indexDocsRemote
-      .replace("var dataURL = 'https://cdn.jsdelivr.net/gh/osmlab/osm-community-index@main/dist/completeFeatureCollection.min.json';", "var dataURL = './completeFeatureCollection.min.json';")
-      .replace("<h2>OSM Communities</h2>", "<h2>OSM Communities</h2><div class='resource'>Local version " + mtime.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " UTC</div>");
-    fs.writeFileSync('docs-local/index.html', indexDocsLocal);
-    fs.copyFileSync('dist/completeFeatureCollection.min.json', 'docs-local/completeFeatureCollection.min.json');
-  }
+  const stats = fs.statSync('dist/completeFeatureCollection.min.json');
+  const mtime = stats.mtime;
+  let indexDocsRemote = fs.readFileSync('docs/index.html').toString();
+
+  let indexDocsLocal = indexDocsRemote
+    .replace("var dataURL = cdnBase", "var dataURL = './'")
+    .replace("<h2>OSM Communities</h2>", "<h2>OSM Communities</h2><div class='resource'>Local version " +
+      mtime.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " UTC</div>");
+
+  fs.ensureFileSync('docs-local/index.html');
+  fs.writeFileSync('docs-local/index.html', indexDocsLocal);
+  fs.copyFileSync('dist/completeFeatureCollection.min.json', 'docs-local/completeFeatureCollection.min.json');
 }
