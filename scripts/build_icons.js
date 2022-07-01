7d817434537d33e1d736e47e64de0520c87f0db7
Bryan Housel <bhousel@gmail.com>
2022-07-01 14:05:06 -0400

    Add `newsletter` type (re: #549)

--- scripts/build_icons.js
+++ scripts/build_icons.js
@@ -1,53 +1,51 @@
-// External
 import chalk from 'chalk';
 import fs from 'node:fs';
-
 import fontawesome from '@fortawesome/fontawesome-svg-core';
+
 import {
-  faAt, faComments, faKeyboard, faLink, faUsers
+  faAt, faComments, faKeyboard, faLink, faNewspaper, faUsers
 } from '@fortawesome/free-solid-svg-icons';
+
 import {
   faDiscord, faDiscourse, faFacebook, faGithub, faGitlab, faLinkedin,
   faMeetup, faReddit, faSlack, faTelegram, faTwitter, faYoutube
 } from '@fortawesome/free-brands-svg-icons';
 
-buildAll();
+const toBuild = {
+  discord: faDiscord,
+  discourse: faDiscourse,
+  facebook: faFacebook,
+  forum: faComments,
+  github: faGithub,
+  gitlab: faGitlab,
+  group: faUsers,
+  irc: faKeyboard,
+  linkedin: faLinkedin,
+  mailinglist: faAt,
+  matrix: faComments,
+  meetup: faMeetup,
+  newsletter: faNewspaper,
+  reddit: faReddit,
+  slack: faSlack,
+  telegram: faTelegram,
+  twitter: faTwitter,
+  url: faLink,
+  youtube: faYoutube
+};
 
-function buildAll() {
-  const faIconMap = {
-    discord: faDiscord,
-    discourse: faDiscourse,
-    facebook: faFacebook,
-    forum: faComments,
-    github: faGithub,
-    gitlab: faGitlab,
-    group: faUsers,
-    irc: faKeyboard,
-    linkedin: faLinkedin,
-    mailinglist: faAt,
-    matrix: faComments,
-    meetup: faMeetup,
-    reddit: faReddit,
-    slack: faSlack,
-    telegram: faTelegram,
-    twitter: faTwitter,
-    url: faLink,
-    youtube: faYoutube
-  };
+const START = '🏗   ' + chalk.yellow('Building icons...');
+const END = '👍  ' + chalk.green('icons built');
 
-  const START = '🏗   ' + chalk.yellow('Building icons...');
-  const END = '👍  ' + chalk.green('icons built');
+console.log('');
+console.log(START);
+console.time(END);
 
-  console.log('');
-  console.log(START);
-  console.time(END);
+for (const [key, value] of Object.entries(toBuild)) {
+  const file = `dist/img/${key}.svg`;
+  console.log(chalk.yellow(file));
 
-  for (let key in faIconMap) {
-    const val = faIconMap[key];
-    const file = `dist/img/${key}.svg`;
-    console.log(chalk.yellow(file));
-    fs.writeFileSync(file, fontawesome.icon(val).html);
-  }
-
-  console.timeEnd(END);
+  // console.log( JSON.stringify(fontawesome.icon(value).html[0], null, 2) );
+  fs.writeFileSync(file, fontawesome.icon(value).html[0]);
 }
+
+console.timeEnd(END);
