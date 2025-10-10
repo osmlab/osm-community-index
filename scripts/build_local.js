47e6a13a96a7c9dfbd91818ea3b2ca18b3612975
Bryan Housel <bhousel@gmail.com>
2025-10-10 16:34:09 -0400

    Can get rid of fs-extra also

--- scripts/build_local.js
+++ scripts/build_local.js
@@ -1,18 +1,18 @@
-import fs from 'fs-extra';
+import fs from 'node:fs';
 
 buildLocal();
 
 function buildLocal() {
   const stats = fs.statSync('dist/completeFeatureCollection.min.json');
   const mtime = stats.mtime;
-  let indexDocsRemote = fs.readFileSync('docs/index.html').toString();
 
-  let indexDocsLocal = indexDocsRemote
+  const indexDocsRemote = fs.readFileSync('docs/index.html', 'utf8');
+  const indexDocsLocal = indexDocsRemote
     .replace("var dataURL = cdnBase", "var dataURL = './'")
     .replace("<h2>OSM Communities</h2>", "<h2>OSM Communities</h2><div class='resource'>Local version " +
       mtime.toISOString().replace(/T/, ' ').replace(/\..+/, '') + " UTC</div>");
 
-  fs.ensureFileSync('docs-local/index.html');
+  fs.mkdirSync('docs-local');
   fs.writeFileSync('docs-local/index.html', indexDocsLocal);
   fs.copyFileSync('dist/completeFeatureCollection.min.json', 'docs-local/completeFeatureCollection.min.json');
 }
